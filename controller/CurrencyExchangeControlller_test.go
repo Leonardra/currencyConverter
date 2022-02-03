c6c0cece7117c1cea892c8a780d77b971862ceec
Oluwatobi Jolayemi <jolayemi.tobi@gmail.com>
2022-02-03 18:01:56 +0100

    Changed CED to GHS and SHI to KSH

--- controller/CurrencyExchangeControlller_test.go
+++ controller/CurrencyExchangeControlller_test.go
@@ -8,7 +8,7 @@ import (
 
 func Test_convertToCurrency(t *testing.T) {
 	exchangeTable := model.CreateExchangeTable()
-	currencyExchange := model.CurrencyExchangeRequestDto{From: "NGN", To: "CED", AmountToConvert: 45000}
+	currencyExchange := model.CurrencyExchangeRequestDto{From: "NGN", To: "GHS", AmountToConvert: 45000}
 	_, result := convertToCurrency(exchangeTable.CurrencyTable, &currencyExchange)
 	assert.Equal(t, 765.00, result)
 }
--- model/CurrencyExchangeTable.go
+++ model/CurrencyExchangeTable.go
@@ -8,9 +8,9 @@ type ExchangeTable struct{
 func CreateExchangeTable() *ExchangeTable{
 	exchangeTable :=new(ExchangeTable)
 	exchangeTable.CurrencyTable = make(map[string]map[string]float64)
-	exchangeTable.CurrencyTable["NGN"] = map[string]float64{"CED": 0.017, "SHI":0.27}
-	exchangeTable.CurrencyTable["CED"] = map[string]float64{"NGN": 60.27, "SHI":16.46}
-	exchangeTable.CurrencyTable["SHI"] = map[string]float64{"NGN": 3.66, "CED":0.061}
+	exchangeTable.CurrencyTable["NGN"] = map[string]float64{"GHS": 0.017, "KSH":0.27}
+	exchangeTable.CurrencyTable["GHS"] = map[string]float64{"NGN": 60.27, "KSH":16.46}
+	exchangeTable.CurrencyTable["KSH"] = map[string]float64{"NGN": 3.66, "GHS":0.061}
 
 	return exchangeTable
 }
